e50b1e6da57de9c6780214623689ecc388e64310
RickSnRz <rickx213@gmail.com>
2023-11-24 23:43:33 -0500

    commit segundo

--- script.js
+++ script.js
@@ -89,3 +89,19 @@ enlaces.forEach(function (enlace) {
     }
   });
 });
+
+let menuVisible = false;
+function mostrarOcultarMenu() {
+  if (menuVisible) {
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+  } else {
+    document.getElementById("nav").classList = "responsive";
+    menuVisible = true;
+  }
+}
+
+function seleccionar() {
+  document.getElementById("nav").classList = "";
+  menuVisible = false;
+}
